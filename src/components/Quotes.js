82d3acb7c6299c41dd2079913a304514f3b09761
rocoscali <rocoscali@gmail.com>
2020-12-19 17:06:19 +0100

    Revert "quotes map + font prefetch"
    This reverts commit 9055ec186027472a0d90bd358a7d93e0022eefaa.

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -18,46 +18,39 @@ const H4 = styled.h4`
 `
 
 const Quotes = () => {
-  const quotesList = [
-    {
-      text: "“Physical fitness is the first requisite of happiness”",
-      author: "- Joseph Pilates",
-    },
-    {
-      text: "“Pilates is complete coordination of body, mind and spirit”",
-      author: "- Joseph Pilates",
-    },
-    {
-      text: "“A man is as young as his spinal columns”",
-      author: "- Joseph Pilates",
-    },
-    {
-      text: "“Change happens through movement and movement heals”",
-      author: "- Joseph Pilates",
-    },
-    {
-      text: "“Every moment of our life can be the beginning of great things”",
-      author: "- Joseph Pilates",
-    },
-    {
-      text: "“It’s the mind itself which shapes the body”",
-      author: "- Joseph Pilates",
-    },
-    {
-      text: "“Everyone is the architect of their own happiness”",
-      author: "- Joseph Pilates",
-    },
-  ]
-
   return (
     <Container>
       <Carousel fade prevIcon={false} nextIcon={false} indicators={false}>
-        {quotesList.map(q => (
-          <Carousel.Item className="text-center p-4" interval={5000}>
-            <H2>{q.text}</H2>
-            <H4>{q.author}</H4>
-          </Carousel.Item>
-        ))}
+        <Carousel.Item className="text-center p-4" interval={5000}>
+          <H2>“Physical fitness is the first requisite of happiness”</H2>
+          <H4>- Joseph Pilates</H4>
+        </Carousel.Item>
+        <Carousel.Item className="text-center  p-4" interval={5000}>
+          <H2>“Pilates is complete coordination of body, mind and spirit”</H2>
+          <H4>- Joseph Pilates</H4>
+        </Carousel.Item>
+        <Carousel.Item className="text-center  p-4" interval={5000}>
+          <H2>“A man is as young as his spinal columns”</H2>
+          <H4>- Joseph Pilates</H4>
+        </Carousel.Item>
+        <Carousel.Item className="text-center  p-4" interval={5000}>
+          <H2>“Change happens through movement and movement heals”</H2>
+          <H4>- Joseph Pilates</H4>
+        </Carousel.Item>
+        <Carousel.Item className="text-center  p-4" interval={5000}>
+          <H2>
+            “Every moment of our life can be the beginning of great things”
+          </H2>
+          <H4>- Joseph Pilates</H4>
+        </Carousel.Item>
+        <Carousel.Item className="text-center  p-4" interval={5000}>
+          <H2>“It’s the mind itself which shapes the body”</H2>
+          <H4>- Joseph Pilates</H4>
+        </Carousel.Item>
+        <Carousel.Item className="text-center  p-4" interval={5000}>
+          <H2>“Everyone is the architect of their own happiness”</H2>
+          <H4>- Joseph Pilates</H4>
+        </Carousel.Item>
       </Carousel>
     </Container>
   )
