e9dc5212135666478d29e2550df74d6832074196
rocoscali <rocoscali@gmail.com>
2021-06-02 09:52:36 +0300

    ifram rel 0

--- src/components/VideoComp.js
+++ src/components/VideoComp.js
@@ -9,6 +9,8 @@ const VideoComp = props => {
         className="embed-responsive-item"
         src={props.videoSrc}
         frameBorder="0"
+        modestbranding="1"
+        rel="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
         webkitallowfullscreen="true"
